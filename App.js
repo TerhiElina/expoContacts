da62bed1fc8c39bd52d6b55ee74f4a893dfacdca
TerhiElina <terhi.evakallio@gmail.com>
2024-03-22 08:36:30 +0200

    contacts and sms

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import * as Contacts from 'expo-contacts';
+import * as SMS from 'expo-sms';
 
 export default function App() {
+ 
+  const [contacts, setContacts] = useState([]);
+  //Create asyncronous function where getContactsAsync function
+  //gets all the contacts from the device
+  //function returns an array of contact objects
+  const getContacts = async () =>{
+    const {status} = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const {data} = await Contacts.getContactsAsync (
+        {fields : [Contacts.Fields.PhoneNumbers]}
+      );
+      if (data.length > 0){
+        setContacts(data); // Tässä asetetaan kaikki yhteystiedot dataan
+      }
+    }
+  }
+  const sendSms = async (phoneNumber, contactName) =>{
+    const isSmsAvailable = await SMS.isAvailableAsync();
+    if (isSmsAvailable  && phoneNumber) {
+      await SMS.sendSMSAsync(phoneNumber, 'Moi ' + contactName)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.buttonContainer}>
+      <Button
+      title="Get Contact"
+      onPress={getContacts} />
+      </View>
+      <FlatList
+        style={styles.list}
+        data={contacts}
+        keyExtractor={(item) => item.id}
+        renderItem = {({item}) => (
+          <View style={styles.listItemContainer}>
+          <Text style={{marginRight:10}}>{item.name}</Text> 
+             {item.phoneNumbers && item.phoneNumbers.length > 0 && (
+              <Text style={{marginRight: 5}}>
+                {item.phoneNumbers[0].number}
+              </Text>
+            )}
+            <View style={{marginRight: 10}}>
+            <Button
+            title='Send SMS'
+            onPress={() => sendSms(item.phoneNumbers[0].number, item.name)} />
+          </View>
+          </View>
+      )}
+      />
+    <StatusBar style="auto" />
+    
     </View>
   );
 }
@@ -14,7 +65,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  list:{
+    marginTop: '10%',
+    marginLeft: '10%',
+  
+
+  },
+  buttonContainer: {
+    width: 200,
+    alignItems:'flex-start',
+    marginTop: '20%',
+    marginLeft: '10%',
+  },
+  listItemContainer: {
+    flexDirection:'row',
+    padding: 5,
+    fontSize:15,
+},
 });
